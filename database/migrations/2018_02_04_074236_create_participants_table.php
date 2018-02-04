d3fd8ac5d75b99d29d9a662faf48aa9d0d6ad769
raghav <raghavpamnani@gmail.com>
2018-02-04 13:51:42 +0530

    migration created

--- database/migrations/2018_02_04_074236_create_participants_table.php
+++ database/migrations/2018_02_04_074236_create_participants_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateParticipantsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,20 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('participants', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('firstname');
             $table->string('middlename');
             $table->string('lastname');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('phone');
-            $table->string('landline');
             $table->boolean('gender');
             $table->date('dob');
-            $table->string('address');
+            $table->string('relation');
             $table->string('avatar');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -38,6 +35,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('participants');
     }
 }
